3cf2540a8f0d41f9e7029208d942b07be4c7d121
galang48 <galangyudha1945@gmail.com>
2024-05-07 11:22:30 +0700

    melakukan pemasukan nilai dan pemanggilan

--- FriendFunctionSatu/FriendFunctionSatu.cpp
+++ FriendFunctionSatu/FriendFunctionSatu.cpp
@@ -8,16 +8,19 @@ private:
 	string nama;
 
 public:
-	friend void setName(mahasiswa& a, string);
+	friend void setNama(mahasiswa& a, string);
 };
 
-void setName(mahasiswa& a, string pNama) 
+void setNama(mahasiswa& a, string pNama) 
 {
 	a.nama = pNama;
 }
+
 int main()
 {
-   
+	mahasiswa joko;
+	setNama(joko, "Kairi Kumar");
+	return 0;
 }
 
 
